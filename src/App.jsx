4d5999ca1cb5fd9ce53cdca397bcfe96155611ab
asadd2723 <asadu3040@gmail.com>
2025-03-22 10:36:07 +0500

    checj

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,17 @@ import { GSM_DATA } from './query/gsmQuery'
 import MapComponent from './MapComponent'
 
 function App() {
+  const apiKey = import.meta.env.GOOGLE_MAP_API
   const { loading, error, data } = useQuery(GSM_DATA, {
     variables: { phoneNumber: '938476' },
   })
   console.log(error, loading, data)
   console.log(data)
+  
   return (
     <>
       <h1>Google Map Integration</h1>
-      <MapComponent apiKey="AIzaSyCzqsAM3u9vLQkCHVd_tvzGRoAf1govjjA" />
+      <MapComponent apiKey={apiKey} />
     </>
   )
 }
